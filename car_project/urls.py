ccddab139f82a1da1dd4cdcefc4e0c4e3e78f4f5
micheal072005 <165802491+micheal072005@users.noreply.github.com>
2025-06-19 12:37:30 +0100

    Update urls.py

--- car_project/urls.py
+++ car_project/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include # type: ignore
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("__reload__/", include("django_browser_reload.urls")),
+    # path("__reload__/", include("django_browser_reload.urls")),
     path('', include('cars.urls')),
     
     
