1a0ae610ae88456811b768e6d292003ff50e364f
micheal072005 <165802491+micheal072005@users.noreply.github.com>
2025-06-19 11:07:34 +0100

    Update settings.py

--- car_project/settings.py
+++ car_project/settings.py
@@ -162,7 +162,7 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = BASE_DIR / 'statictfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
