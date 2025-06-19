c5ea4ebb137bc4346806652d385c4298476b7efa
micheal072005 <165802491+micheal072005@users.noreply.github.com>
2025-06-19 11:53:21 +0100

    Update settings.py

--- car_project/settings.py
+++ car_project/settings.py
@@ -56,7 +56,7 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
-NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
+# NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
